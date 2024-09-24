42e256baf43507c46ad688b4edd954998abe3ed2
bdvalero98 <danilovalero25@gmail.com>
2024-09-24 13:23:09 -0500

    Se agrega proyecto base

---
+++ fondos/src/test/java/com/btg/pactual/fondos/FondosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.pactual.fondos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
